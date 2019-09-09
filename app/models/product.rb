7e24a1c09db0c84fad606e0ff8446e4315069aaa
Sumejja Halilović <sumejjaha@maestralsolutions.com>
2019-09-09 20:02:48 +0200

    work in progress

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,20 @@
 class Product < ApplicationRecord
+
+  COLORS_LIST = ["red", "black", "green", "grey", "white"]
     
 mount_uploader :image, ImageUploader
 
 def self.search(search)
-	if search
-		self.where(name: search)
+  if search
+    result = [];
+
+    Product.all.each do |product|
+      if product.name.downcase.include? search.downcase
+        result << product
+      end
+    end
+
+    result
 	else
 		Product.all
 	end
